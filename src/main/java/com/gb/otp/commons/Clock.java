c0a2beec9f184ce79bb07d14b33770e0c20c35a5
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-08-28 23:03:20 +0530

    added  clock to get current interval

--- src/main/java/com/gb/otp/commons/Clock.java
+++ src/main/java/com/gb/otp/commons/Clock.java
@@ -1,12 +1,15 @@
 package com.gb.otp.commons;
 
 import java.time.Instant;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import static com.gb.otp.constants.AppConstant.INTERVAL;
 
 public class Clock {
     private final int interval;
-
+    Calendar calendar;
     public Clock(int interval) {
         this.interval = interval;
     }
@@ -15,8 +18,14 @@ public class Clock {
         this.interval = INTERVAL;
     }
 
+    /**
+     * the clock here and the clock from client should be in sync so use UTC from both ends.
+     * Could use Zoned date time too based on the client's zone store the details.
+     * @return long
+     */
     public long getCurrentInterval() {
-        long currentTimeInMills = Instant.now().toEpochMilli() / 1000;
+        calendar = GregorianCalendar.getInstance(TimeZone.getTimeZone("UTC"));
+        long currentTimeInMills = calendar.getTimeInMillis() / 1000;
         return currentTimeInMills / interval;
     }
 
