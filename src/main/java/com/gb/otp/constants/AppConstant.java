98d1735fa8f272edfd24b726d121428a45954dfe
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-08-29 20:37:55 +0530

    added app cnstants

---
+++ src/main/java/com/gb/otp/constants/AppConstant.java
@@ -0,0 +1,9 @@
+package com.gb.otp.constants;
+
+public class AppConstant {
+    public static final short INTERVAL = 2;
+    public static final int SECRET_SIZE = 10;
+    public static final String SEPARATOR = "-";
+    private AppConstant() {
+    }
+}
